e4f3a3c4e2a9b48a0445b679eca2fdd3cbc447a9
Oleksii R <sorrowerkein+bitbucket@gmail.com>
2024-06-04 02:03:30 +0200

    update

--- src/ios/MySpeed.swift
+++ src/ios/MySpeed.swift
@@ -12,14 +12,14 @@ import CoreLocation
     var mLocationManager: CLLocationManager?
     var lastLocation: CLLocation?
     var currentCallbackId : String?
-    
+
     /**
      * Check device for ApplePay capability
      */
     @objc(get:) func get(command: CDVInvokedUrlCommand){
        let callbackID = command.callbackId;
         currentCallbackId = callbackID
-        
+
         if(mLocationManager == nil) {
             mLocationManager = CLLocationManager()
             mLocationManager!.delegate = self
@@ -31,33 +31,49 @@ import CoreLocation
             mLocationManager!.startMonitoringSignificantLocationChanges()
             mLocationManager!.requestLocation()
         }
-        
+
         if lastLocation != nil  {
-            // Get the speed in meters per second
+                    // Get the speed in meters per second
             let speed = lastLocation!.speed < 0 ? 0 : lastLocation!.speed;
             // Convert the speed to kilometers per hour
             let speedInKmPerHour = speed * 3.6
-            
-            let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: String(speedInKmPerHour))
-            commandDelegate.send(result, callbackId: callbackID)
+
+            let locStruct = LocStuct(lat: lastLocation!.coordinate.latitude, lng: lastLocation!.coordinate.longitude, speed: speedInKmPerHour)
+
+            do {
+                let jsonData = try JSONEncoder().encode(locStruct)
+                let jsonString = String(data: jsonData, encoding: .utf8)!
+
+                let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: jsonString)
+                commandDelegate.send(result, callbackId: callbackID)
+
+            } catch { print(error) }
+
+
         } else {
             failWithError("Location Not available")
         }
-        
-        
+
+
     }
-   
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         lastLocation = locations[0]
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Swift.Error) {
         print(error)
     }
-    
+
     private func failWithError(_ error: String){
         let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error)
         commandDelegate.send(result, callbackId: currentCallbackId)
     }
 }
 
+
+struct LocStuct : Encodable {
+    var lat: Double
+    var lng: Double
+    var speed: Double
+}
